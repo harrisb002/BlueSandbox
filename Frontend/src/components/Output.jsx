c6a04986331de6018890ea6d91c11316bf007e22
harrisb002 <harrisbe@sonoma.edu>
2024-04-04 02:34:36 -0700

    Adding output section

---
+++ Frontend/src/components/Output.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Box, Button, Text, useToast } from "@chakra-ui/react";
+
+const Output = () => {
+  const [output, setOutput] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  return (
+    <Box w="50%">
+      <Text mb={2} fontSize="lg">
+        Output
+      </Text>
+      <Button
+        variant="outline"
+        colorScheme="green"
+        mb={4}
+      >
+        Run Code
+      </Button>
+      <Box
+        height="75vh"
+        p={2}
+        color={isError ? "red.400" : ""}
+        border="1px solid"
+        borderRadius={4}
+        borderColor={isError ? "red.500" : "#333"}
+      >
+      </Box>
+    </Box>
+  );
+};
+export default Output;
--- Frontend/src/components/Sandbox.jsx
+++ Frontend/src/components/Sandbox.jsx
@@ -1,8 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import React, { useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../languages";
+import Output from "./Output";
 
 const Sandbox = () => {
   const editorRef = useRef();
@@ -21,18 +22,27 @@ const Sandbox = () => {
 
   return (
     <Box>
-      <LanguageSelector language={language} onSelect={onSelect} />
-      <Editor
-        height="75vh"
-        theme="vs-dark"
-        language={language}
-        defaultValue={CODE_SNIPPETS[language]}
-        onMount={onMount}
-        value={value}
-        onChange={(value) => setValue(value)}
-      />
+      <HStack spacing={4}>
+        <Box w="50%">
+          <LanguageSelector language={language} onSelect={onSelect} />
+          <Editor
+            options={{
+              minimap: {
+                enabled: false,
+              },
+            }}
+            height="75vh"
+            theme="vs-dark"
+            language={language}
+            defaultValue={CODE_SNIPPETS[language]}
+            onMount={onMount}
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+        </Box>
+        <Output/>
+      </HStack>
     </Box>
   );
 };
-
 export default Sandbox;
