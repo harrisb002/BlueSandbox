2c100aebc1fb1374a3fd290711a56339846fa9be
harrisb002 <harrisbe@sonoma.edu>
2024-04-04 01:20:10 -0700

    Creating langauge selection

--- Frontend/src/components/Sandbox.jsx
+++ Frontend/src/components/Sandbox.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import React, { useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
+import LanguageSelector from "./LanguageSelector";
 
 const Sandbox = () => {
   const editorRef = useRef();
@@ -13,12 +14,14 @@ const Sandbox = () => {
 
   return (
     <Box>
+    <LanguageSelector/>
       <Editor
         height="75vh"
         theme="vs-dark"
         defaultLanguage="javascript"
         defaultValue="// some comment"
         onMount={onMount}
+        value={value}
       />
     </Box>
   );
